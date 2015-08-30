443bf505164d0626c2c6f9c1b9baa0f705b9afca
AnnaKovalek <anna.kovalek@gmail.com>
2015-08-30 22:25:17 +0300

    fix

--- app/src/main/java/com/inthecheesefactory/lab/designlibrary/MainActivity.java
+++ app/src/main/java/com/inthecheesefactory/lab/designlibrary/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
-        collapsingToolbarLayout.setTitle("Design Library");
+        collapsingToolbarLayout.setTitle("Android Developer");
     }
 
     @Override
